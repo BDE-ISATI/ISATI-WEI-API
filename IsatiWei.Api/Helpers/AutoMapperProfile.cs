2185423e6ea2613bec279790345b9362f9e2e677
Feldrise <admin@feldrise.com>
2020-07-23 09:41:39 +0200

    [Add] Auto Mapper profile

---
+++ IsatiWei.Api/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using IsatiWei.Api.Models;
+using IsatiWei.Api.Models.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsatiWei.Api.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserRegister, User>();
+            CreateMap<UserLogin, User>();
+        }
+    }
+}
